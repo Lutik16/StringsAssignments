5e5350998dbed637c4cd1955471260b5b8d3f4a9
Lucy <luda.silina@gmail.com>
2017-04-06 17:47:16 +0100

    Add Part3 file

--- Main.java
+++ Main.java
@@ -3,9 +3,11 @@
  */
 public class Main {
     public static void main (String[] args){
-        /*Part1 test = new Part1();
-        test.test();*/
-        Part2 test =new Part2();
-        test.testHowMany();
+        /*Part1 test1 = new Part1();
+        test1.test();
+        Part2 test2 =new Part2();
+        test2.testHowMany();*/
+        Part3 test3 = new Part3();
+        test3.testCountGenes();
     }
 }
---
+++ Part3.java
@@ -0,0 +1,146 @@
+/**
+ * Created by lucy on 06/04/17.
+ */
+public class Part3 {
+    public int findStopCodon (String dna, int startIndex, String stopCodon){
+        int currIndex = dna.indexOf(stopCodon, startIndex+3);
+        while (currIndex !=-1){
+            int diff = currIndex-startIndex;
+            if(diff%3==0){
+                return currIndex;
+            }
+            else{
+                currIndex = dna.indexOf(stopCodon, currIndex+1);
+            }
+        }
+        return dna.length();
+    }
+
+   /* public void testFindStopCodon (){
+        //            012345678901234567890123456789012345678901234
+        String dna = "XXXYYYZZZTAAXXXYYYZTAAXXTAAXXXTGAYYYTAAZZZXXX";
+        int testIndex = findStopCodon(dna,0, "TAA");
+        if(testIndex!=9){
+            System.out.println("error on 9");
+        }
+        testIndex = findStopCodon(dna, 9, "TAA");
+        if(testIndex!=24){
+            System.out.println("error on 24");
+        }
+        testIndex= findStopCodon(dna,24,"TAA");
+        if(testIndex!=36){
+            System.out.println("error on 36");
+        }
+        testIndex = findStopCodon(dna, 36, "TAA");
+        if(testIndex!=45){
+            System.out.println("error on 45");
+        }
+        testIndex = findStopCodon(dna,0, "TGA");
+        if(testIndex!=30){
+            System.out.println("error on 30");
+        }
+
+        System.out.println("Test finished");
+    }*/
+
+    public String findGene (String dna, int where){
+        int startIndex = dna.indexOf("ATG", where);
+        if(startIndex==-1){
+            return "";
+        }
+        int taaIndex = findStopCodon(dna, startIndex, "TAA");
+        int tagIndex = findStopCodon(dna, startIndex,"TAG");
+        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
+        int minIndex = Math.min(taaIndex, Math.min(tagIndex,tgaIndex));
+        if(minIndex==dna.length()){
+            return "";
+        }
+        return dna.substring(startIndex, minIndex+3);
+    }
+
+    /* public void testFindGene(){
+        //            01234567890123456789
+        String dna = "xxxATGyyyzzzTAA";
+        String gene = findGene(dna, 0);
+        if(gene.equalsIgnoreCase("ATGyyyzzzTAA")) {
+            System.out.println("DNA string is " + " " + dna);
+            System.out.println("Gene is " + " " + gene);
+        }
+        else {
+            System.out.println("error 1 (TAA)");
+        }
+
+        dna = "GTTCAATAGCGGTAATTTCCC";
+        gene = findGene(dna, 0);
+        if(!gene.equalsIgnoreCase("")){
+            System.out.println("error 2 (no ATG)");
+        }
+        System.out.println("DNA string is"+" "+ dna);
+        System.out.println("Gene is "+ gene);
+
+        dna = "ATGxxxyyyzTAAxxTAGzzzTGAxxxTAA";
+        gene = findGene(dna, 0);
+        if(!gene.equalsIgnoreCase("ATGxxxyyyzTAAxxTAG")) {
+            System.out.println("error 3 (multiple stop codons)");
+        }
+        System.out.println("DNA string is"+" "+ dna);
+        System.out.println("Gene is "+ gene);
+
+        dna = "ATGTTTCCCAAAGTACTACTAA";
+        gene = findGene(dna, 0);
+        if(!gene.equalsIgnoreCase("")){
+            System.out.println("error 4 (no stopCodon)");
+        }
+        System.out.println("DNA string is"+" "+ dna);
+        System.out.println("Gene is "+ gene);
+    }*/
+
+    public void printAllGenes (String dna){
+        int startIndex = 0;
+        while (true){
+            String currentGene = findGene(dna, startIndex);
+            if (currentGene.isEmpty()){
+                break;
+            }
+            System.out.println(currentGene);
+            startIndex = (dna.indexOf(currentGene,startIndex)+ currentGene.length());
+        }
+    }
+
+    public int countGenes (String dna){
+        int startIndex = 0;
+        int numberOfGenes = 0;
+        while (true){
+            String currentGene = findGene(dna, startIndex);
+            if (currentGene.isEmpty()){
+                break;
+            }
+            numberOfGenes = numberOfGenes+1;
+            startIndex = (dna.indexOf(currentGene,startIndex)+ currentGene.length());
+        }
+        return numberOfGenes;
+    }
+
+    public void testCountGenes(){
+        String dna = "ACATGCCCTGAGGATGTTTTCGTAAATGATGTAGCCATG";
+        int howMany = countGenes(dna);
+        if (howMany!=3){
+            System.out.println("error on 3");
+        }
+        System.out.println("The number of genes in "+ dna+ " is "+howMany);
+
+        dna = "AAACCCTTTTGA";
+        howMany = countGenes(dna);
+        if(howMany!=0){
+            System.out.println("error on 0");
+        }
+        System.out.println("The number of genes in "+ dna+ " is "+howMany);
+
+        dna = "ATGTGAcccATGTTTATCTAAggATGTCCGGGAAATAGaaaATGTAGaaggccATGATGATGTAAcgATGCCCTGACGATGA";
+        howMany = countGenes(dna);
+        if(howMany!=6){
+            System.out.println("error on 6");
+        }
+        System.out.println("The number of genes in "+ dna+ " is "+howMany);
+    }
+}
