b77871e11a38c0affb1845324e117df344653a08
Lucy <luda.silina@gmail.com>
2017-04-26 11:58:16 +0100

    change Part3: where-index.delete comments

--- Part3.java
+++ Part3.java
@@ -16,35 +16,8 @@ public class Part3 {
         return dna.length();
     }
 
-   /* public void testFindStopCodon (){
-        //            012345678901234567890123456789012345678901234
-        String dna = "XXXYYYZZZTAAXXXYYYZTAAXXTAAXXXTGAYYYTAAZZZXXX";
-        int testIndex = findStopCodon(dna,0, "TAA");
-        if(testIndex!=9){
-            System.out.println("error on 9");
-        }
-        testIndex = findStopCodon(dna, 9, "TAA");
-        if(testIndex!=24){
-            System.out.println("error on 24");
-        }
-        testIndex= findStopCodon(dna,24,"TAA");
-        if(testIndex!=36){
-            System.out.println("error on 36");
-        }
-        testIndex = findStopCodon(dna, 36, "TAA");
-        if(testIndex!=45){
-            System.out.println("error on 45");
-        }
-        testIndex = findStopCodon(dna,0, "TGA");
-        if(testIndex!=30){
-            System.out.println("error on 30");
-        }
-
-        System.out.println("Test finished");
-    }*/
-
-    public String findGene (String dna, int where){
-        int startIndex = dna.indexOf("ATG", where);
+    public String findGene (String dna, int index){
+        int startIndex = dna.indexOf("ATG", index);
         if(startIndex==-1){
             return "";
         }
@@ -57,44 +30,7 @@ public class Part3 {
         }
         return dna.substring(startIndex, minIndex+3);
     }
-
-    /* public void testFindGene(){
-        //            01234567890123456789
-        String dna = "xxxATGyyyzzzTAA";
-        String gene = findGene(dna, 0);
-        if(gene.equalsIgnoreCase("ATGyyyzzzTAA")) {
-            System.out.println("DNA string is " + " " + dna);
-            System.out.println("Gene is " + " " + gene);
-        }
-        else {
-            System.out.println("error 1 (TAA)");
-        }
-
-        dna = "GTTCAATAGCGGTAATTTCCC";
-        gene = findGene(dna, 0);
-        if(!gene.equalsIgnoreCase("")){
-            System.out.println("error 2 (no ATG)");
-        }
-        System.out.println("DNA string is"+" "+ dna);
-        System.out.println("Gene is "+ gene);
-
-        dna = "ATGxxxyyyzTAAxxTAGzzzTGAxxxTAA";
-        gene = findGene(dna, 0);
-        if(!gene.equalsIgnoreCase("ATGxxxyyyzTAAxxTAG")) {
-            System.out.println("error 3 (multiple stop codons)");
-        }
-        System.out.println("DNA string is"+" "+ dna);
-        System.out.println("Gene is "+ gene);
-
-        dna = "ATGTTTCCCAAAGTACTACTAA";
-        gene = findGene(dna, 0);
-        if(!gene.equalsIgnoreCase("")){
-            System.out.println("error 4 (no stopCodon)");
-        }
-        System.out.println("DNA string is"+" "+ dna);
-        System.out.println("Gene is "+ gene);
-    }*/
-
+    
     public void printAllGenes (String dna){
         int startIndex = 0;
         while (true){
